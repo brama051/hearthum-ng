fb745c4abae1179198ba8676cbf031d56cd993b6
Vedran Abramovic <vedran.abramovic@ag04.com>
2017-06-05 22:02:40 +0200

    Implemented bootstrap 4 powered by bootstrap 4 directives

--- src/app/layout/recording-history/recording-history.module.ts
+++ src/app/layout/recording-history/recording-history.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { RecordingHistoryRoutingModule } from './recording-history-routing.module';
 import { RecordingHistoryComponent } from './recording-history.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
       CommonModule,
-      RecordingHistoryRoutingModule
+      RecordingHistoryRoutingModule,
+      NgbModule.forRoot(),
+
   ],
   declarations: [RecordingHistoryComponent]
 })
