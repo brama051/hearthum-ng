a27c4d0eecf0355c8d335b53f5c69a4e05c79bef
Vedran Abramovic <vedran.abramovic@ag04.com>
2017-06-09 00:45:12 +0200

    initial version of the recorder

--- src/app/layout/recorder/recorder.component.ts
+++ src/app/layout/recorder/recorder.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+declare const navigator: any;
+declare const MediaRecorder: any;
 
 @Component({
     selector: 'app-recorder',
@@ -10,20 +12,49 @@ export class RecorderComponent implements OnInit {
     public recordingStarted = false;
     public recordingFinished = false;
     public playingActive = false;
+    // --
+    private chunks: any = [];
+    private mediaRecorder: any;
 
     constructor() {
-        this.audio.src = 'https://www.freesound.org/data/previews/146/146765_1417288-lq.mp3';
     }
 
     ngOnInit() {
+        const onSuccess = stream => {
+            this.mediaRecorder = new MediaRecorder(stream);
+            this.mediaRecorder.onstop = e => {
+                this.audio = new Audio();
+                this.audio.onended = er => {
+                    console.log('kraj snimke');
+                    console.log(er);
+                    this.playingActive = false;
+                }
+                const blob = new Blob(this.chunks, { 'type': 'audio/wav;' });
+                this.chunks.length = 0;
+                this.audio.src = window.URL.createObjectURL(blob);
+                this.audio.load();
+            };
+
+            this.mediaRecorder.ondataavailable = e => this.chunks.push(e.data);
+        };
+
+        navigator.getUserMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+        navigator.getUserMedia({ audio: true }, onSuccess, e => console.log(e));
+
     }
 
     public cmdStartRecording() {
         this.recordingStarted = true;
+        this.mediaRecorder.start();
     }
 
     public cmdStopRecording() {
         this.recordingFinished = true;
+        this.mediaRecorder.stop();
     }
 
     public cmdPlayerPlay() {
